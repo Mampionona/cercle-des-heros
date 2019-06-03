842ae94079236914b1e49a2070df43169b473e04
Mampionona <devke03@gmail.com>
2019-06-03 10:50:24 +0300

    handle anchor links

--- themes/cercle-theme/assets/js/core/menu.js
+++ themes/cercle-theme/assets/js/core/menu.js
@@ -1,29 +1,27 @@
 import ScrollToElement from 'scroll-to-element';
 
-const options = {
-  duration: 1000,
-  // See https://github.com/component/ease for more
-  ease: 'out-quint',
-  offset: 0
-};
-
 (function (w, d, undefined) {
   d.addEventListener('DOMContentLoaded', () => {
-    const toggler = d.getElementById('navbar-toggler');
-    const anchor = (anchor) =>
-      anchor.addEventListener('click', (e) => {
-        const hash = anchor.hash;
-        if (hash && anchor.pathname === w.location.pathname) {
-          toggler.classList.remove('is-active');
-          d.body.classList.remove('menu-open');
-          ScrollToElement(hash, options);
-          e.preventDefault();
-        }
-      });
     // Handle navbar-toggler click
+    const toggler = d.getElementById('navbar-toggler');
     toggler.addEventListener('click', () => {
       toggler.classList.toggle('is-active');
       d.body.classList.toggle('menu-open');
+    });
+
+    // anchors
+    const anchor = anchor => anchor.addEventListener('click', (e) => {
+      const hash = anchor.hash;
+      if (hash && anchor.pathname === w.location.pathname) {
+        e.preventDefault();
+        toggler.classList.remove('is-active');
+        d.body.classList.remove('menu-open');
+        ScrollToElement(hash, {
+          duration: 1500,
+          ease: 'out-quint',
+          offset: 0
+        });
+      }
     });
     d.querySelectorAll('a[href*="#"]').forEach(anchor);
   });
