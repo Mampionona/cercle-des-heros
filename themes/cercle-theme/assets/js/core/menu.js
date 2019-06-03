00ec6a38573fa0a0082edfaa30a98daaf31756df
Mampionona <devke03@gmail.com>
2019-06-03 09:48:47 +0300

    handle anchor links

--- themes/cercle-theme/assets/js/core/menu.js
+++ themes/cercle-theme/assets/js/core/menu.js
@@ -1,10 +1,30 @@
+import ScrollToElement from 'scroll-to-element';
+
+const options = {
+  duration: 1000,
+  // See https://github.com/component/ease for more
+  ease: 'out-quint',
+  offset: 0
+};
+
 (function (w, d, undefined) {
   d.addEventListener('DOMContentLoaded', () => {
-    // Handle navbar-toggler click
     const toggler = d.getElementById('navbar-toggler');
+    const anchor = (anchor) =>
+      anchor.addEventListener('click', (e) => {
+        const hash = anchor.hash;
+        if (hash && anchor.pathname === w.location.pathname) {
+          toggler.classList.remove('is-active');
+          d.body.classList.remove('menu-open');
+          ScrollToElement(hash, options);
+          e.preventDefault();
+        }
+      });
+    // Handle navbar-toggler click
     toggler.addEventListener('click', () => {
       toggler.classList.toggle('is-active');
       d.body.classList.toggle('menu-open');
     });
+    d.querySelectorAll('a[href*="#"]').forEach(anchor);
   });
 } (window, document));
