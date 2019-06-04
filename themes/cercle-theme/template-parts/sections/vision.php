cb247e874623d47d082d6de2f7870776aed31b35
Mampionona <devke03@gmail.com>
2019-06-04 17:36:24 +0300

    end of day

--- themes/cercle-theme/template-parts/sections/vision.php
+++ themes/cercle-theme/template-parts/sections/vision.php
@@ -16,21 +16,23 @@ $backgroundImage = get_field('background_image');
       </div>
     </div>
   </div>
-  <div class="icons">
-    <div class="d-flex justify-content-center flex-column flex-lg-row align-items-center align-items-lg-center">
-      <?php foreach ($icons as $icon) : ?>
-        <?php if (!$icon['active']) continue; ?>
-        <div class="icon-item text-center">
-          <div class="d-inline-block">
-            <div class="icon d-flex align-items-center justify-content-center">
-              <img src="<?php echo wp_get_attachment_url($icon['icon']); ?>" alt>
+  <?php if ($icons) : ?>
+    <div class="icons">
+      <div class="d-flex justify-content-center flex-column flex-lg-row align-items-center align-items-lg-center">
+        <?php foreach ($icons as $icon) : ?>
+          <?php if (!$icon['active']) continue; ?>
+          <div class="icon-item text-center">
+            <div class="d-inline-block">
+              <div class="icon d-flex align-items-center justify-content-center">
+                <img src="<?php echo wp_get_attachment_url($icon['icon']); ?>" alt>
+              </div>
             </div>
+            <div class="text"><?php echo $icon['text']; ?></div>
           </div>
-          <div class="text"><?php echo $icon['text']; ?></div>
-        </div>
-      <?php endforeach; ?>
+        <?php endforeach; ?>
+      </div>
     </div>
-  </div>
+  <?php endif; ?>
   <div class="post-thumbnail">
     <?php the_post_thumbnail('vision-thumb', array('class' => 'img-fluid')); ?>
   </div>
